68446db67cfedaba61e12a3d67cf3c5fc150899a
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-12 11:07:39 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -1,11 +1,4 @@
 #STREAMLIT APP 1 !!!!!!!!!!!!!!!!
 
-import pandas as pd
-import numpy as np
 import streamlit as st
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+st.text("This is some text")
