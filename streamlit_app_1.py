f3b0051fc847dc878bb7e5c0bcd08fde58ec7529
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-12 11:37:24 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -10,6 +10,7 @@ num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',a)
 date_opt = st.multiselect('What are your datetime columns',a)
 st.write('Numerical columns:',num_opt,'Categorical columns:',cat_opt,'date columns:',date_opt)
+print('Numerical columns:',num_opt,'Categorical columns:',cat_opt,'date columns:',date_opt)
 
 
 
