ca5efacb89a0492cdcd518f6ac7de49db664aee9
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-12 11:18:47 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -2,3 +2,9 @@
 
 import streamlit as st
 st.text("ML APP1")
+
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+st.write('You selected:', options)
