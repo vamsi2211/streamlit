2c276d47d5406af3e83098ae255618b68a443255
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-16 22:33:05 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -12,5 +12,17 @@ num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
 date_opt = st.multiselect('What are your datetime columns',[x for x in a if x not in num_opt+cat_opt])
 st.write('Numerical columns:',num_opt,'Categorical columns:',cat_opt,'date columns:',date_opt)
+with st.form(key='columns_in_form'):
+    c1, c2, c3, c4 = st.columns(4)
+    with c1:
+        initialInvestment = st.text_input("Starting capital",value=500)
+    with c2:
+        monthlyContribution = st.text_input("Monthly contribution (Optional)",value=100)
+    with c3:
+        annualRate = st.text_input("Annual increase rate in percentage",value="15")
+    with c4:
+        investingTimeYears = st.text_input("Duration in years:",value=10)
+
+    submitButton = st.form_submit_button(label = 'Calculate')
 
 
