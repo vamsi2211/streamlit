a0ef04b0d330ffb831a4ef98f4aa21672b8b1e6a
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-16 20:12:46 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -6,7 +6,7 @@ import numpy as np
 
 st.set_page_config(page_title="*BHANU.............................",page_icon="🧑‍💻", layout="centered")
 st.text("ML APP1")
-train = pd.DataFrame({'col1':list(range(10)),'col2':'a b c d e f g h i j'.split(' '),'col3':'A B C D E F G H J H I J'.split(' ')})
+train = pd.DataFrame({'col1':list(range(10)),'col2':'a b c d e f g h i'.split(' '),'col3':'A B C D E F G H I'.split(' ')})
 a = train.columns
 num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
