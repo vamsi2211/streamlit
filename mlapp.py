1b9454bd528928c5e03e866f686ac7ba2de9fdcc
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-19 12:18:49 +0530

    Update mlapp.py

--- mlapp.py
+++ mlapp.py
@@ -6,7 +6,7 @@ import numpy as np
 
 st.set_page_config(page_title="MLAPP",page_icon="🧑‍💻", layout="centered")
 st.text("ML APP1")
-train = pd.DataFrame({'col1':list(range(10)),'col2':list(range(10)),'col3':list(range(10)),'col3':list('A','B','C','D','E','F','G','H','I')})
+train = pd.DataFrame({'col1':list(range(10)),'col2':list(range(10)),'col3':list(range(10)),'col6':[chr(x) for x in range(65,75)], 'col5':[chr(x) for x in range(97,97+10)]})
 a = train.columns
 num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
