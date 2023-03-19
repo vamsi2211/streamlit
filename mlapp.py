c93df3b74e47c9db329bafd8eae56683030eefa8
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-19 12:06:27 +0530

    Update mlapp.py

--- mlapp.py
+++ mlapp.py
@@ -12,13 +12,5 @@ num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
 date_opt = st.multiselect('What are your datetime columns',[x for x in a if x not in num_opt+cat_opt])
 st.write('Numerical columns:',num_opt,'Categorical columns:',cat_opt,'date columns:',date_opt)
-
-    initialInvestment = st.text_input("Starting capital",value=500)
-    monthlyContribution = st.text_input("Monthly contribution (Optional)",value=100)
-    annualRate = st.text_input("Annual increase rate in percentage",value="15")
-    investingTimeYears = st.text_input("Duration in years:",value=10)
-    sel1= form.selectbox("Report Type", ("normal", "full"))
-    track= form.text_input("enter track no").upper()
-
 if submit:
     st.success('The output is this x')
