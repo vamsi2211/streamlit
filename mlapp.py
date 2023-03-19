84ef4cb5aacb3b9afaaef7ae65d5f049d689d4ed
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-19 12:22:51 +0530

    Update mlapp.py

--- mlapp.py
+++ mlapp.py
@@ -1,13 +1,17 @@
-#STREAMLIT APP 1 !!!!!!!!!!!!!!!!
+#MLAPP 1 !!!!!!!!!!!!!!!!
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="MLAPP",page_icon="🧑‍💻", layout="centered")
 st.text("ML APP1")
-train = pd.DataFrame({'col1':list(range(10)),'col2':list(range(10)),'col3':list(range(10)),'col5':[chr(x) for x in range(65,75)], 'col6':[chr(x) for x in range(97,97+10)]})
+train = pd.DataFrame({'col1':list(range(10)),
+                      'col2':list(range(10)),
+                      'col3':list(range(10)),
+                      'col5':[chr(x) for x in range(65,75)],
+                      'col6':[chr(x) for x in range(97,97+10)]})
+st.dataframe(train.head, use_container_width=True)
 a = train.columns
 num_opt = st.multiselect('What are your numerical columns',a)
 cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
