ca2a3246ff980ae052be9d1fb417376f280a5e63
vamsi2211 <125970538+vamsi2211@users.noreply.github.com>
2023-03-20 15:14:56 +0530

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -1,52 +1,4 @@
 # #STREAMLIT APP 1 !!!!!!!!!!!!!!!!
-
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-
-# st.set_page_config(page_title="*BHANU.............................",page_icon="🧑‍💻", layout="centered")
-# st.text("ML APP1")
-# train = pd.DataFrame({'col1':list(range(10)),'col2':list(range(10)),'col3':list(range(10))})
-# a = train.columns
-# num_opt = st.multiselect('What are your numerical columns',a)
-# cat_opt = st.multiselect('What are your categorical columns',[x for x in a if x not in num_opt])
-# date_opt = st.multiselect('What are your datetime columns',[x for x in a if x not in num_opt+cat_opt])
-# st.write('Numerical columns:',num_opt,'Categorical columns:',cat_opt,'date columns:',date_opt)
-# with st.form(key='columns_in_form'):
-#     c1, c2, c3, c4 = st.columns(4)
-#     with c1:
-#         initialInvestment = st.text_input("Starting capital",value=500)
-#     with c2:
-#         monthlyContribution = st.text_input("Monthly contribution (Optional)",value=100)
-#     with c3:
-#         annualRate = st.text_input("Annual increase rate in percentage",value="15")
-#     with c4:
-#         investingTimeYears = st.text_input("Duration in years:",value=10)
-
-#     submitButton = st.form_submit_button(label = 'Calculate')
-
-# form = st.form(key="my-form")
-# c1, c2 = st.columns(2)
-# with c1:
-#     sel1= form.selectbox("Report Type", ("normal", "full"))
-# with c2:
-#     track= form.text_input("enter track no").upper()
-# submit = form.form_submit_button("Generate Report")
-
-# if submit:
-#     st.success('The output is this x')
-
-
-# import sys
-# import subprocess
-
-# # implement pip as a subprocess:
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-# 'streamlit-lottie'])
-
-
-# !pip install streamlit-lottie
-
 import pickle
 import streamlit as st 
 
@@ -209,8 +161,26 @@ def main():
             VehicleYOM = st.text_input("VehicleYOM")
         submitButton = st.form_submit_button(label = 'Submit9')
 
+  test = pd.DataFrame([CustomerID,DateOfIncident,TypeOfIncident,TypeOfCollission,AuthoritiesContacted,IncidentState,IncidentCity,IncidentAddress,IncidentTime,NumberOfVehicles,\
+  PropertyDamage,BodilyInjuries,Witnesses,PoliceReport,AmountOfTotalClaim,AmountOfInjuryClaim,InsuredAge,InsuredZipCode,InsuredGender,InsuredEducationLevel,InsuredOccupation,\
+  InsuredHobbies,CapitalGains,CapitalLoss,Country,InsurancePolicyNumber,CustomerLoyaltyPeriod,DateOfPolicyCoverage,InsurancePolicyState,Policy_CombinedSingleLimit,Policy_Deductible,\
+  PolicyAnnualPremium,UmbrellaLimit,InsuredRelationship,ReportedFraud,VehicleID,VehicleModel,VehicleMake,VehicleYOM],\
+               columns='CustomerID,DateOfIncident,TypeOfIncident,TypeOfCollission,AuthoritiesContacted,IncidentState,IncidentCity,IncidentAddress,IncidentTime,NumberOfVehicles,\
+  PropertyDamage,BodilyInjuries,Witnesses,PoliceReport,AmountOfTotalClaim,AmountOfInjuryClaim,InsuredAge,InsuredZipCode,InsuredGender,InsuredEducationLevel,InsuredOccupation,\
+  InsuredHobbies,CapitalGains,CapitalLoss,Country,InsurancePolicyNumber,CustomerLoyaltyPeriod,DateOfPolicyCoverage,InsurancePolicyState,Policy_CombinedSingleLimit,Policy_Deductible,\
+  PolicyAnnualPremium,UmbrellaLimit,InsuredRelationship,ReportedFraud,VehicleID,VehicleModel,VehicleMake,VehicleYOM'.split(','))
+  st.dataframe(test, use_container_width=True)
 
     
+ # assigning int and removing insured zip code from int float cols to num_cols
+
+# int_cols = list(df_train_final.select_dtypes(include = "int").columns)
+# int_cols.remove("InsuredZipCode")
+# int_cols.remove("InsurancePolicyNumber")
+
+
+# float_cols = list(df_train_final.select_dtypes(include = "float").columns)
+# num_cols = int_cols+float_cols
 
 
     result=""
